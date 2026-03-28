5bf3ec2eb50a5748ccb648ca0cac32c1d8c5c6f2
rishab <rishab@creditoracademy.com>
2026-03-28 14:27:54 +0530

    api calling

--- frontend/src/services/Admin/admin.js
+++ frontend/src/services/Admin/admin.js
@@ -1,7 +1,7 @@
 import axios from "axios";
-
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://localhost:5000";
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/presentation"
+  baseURL: `${BASE_URL}/api/presentation` 
 });
 
 // 🔥 Add interceptor to attach token
